b36b6d14183eeee5726a046755411e4d8424fdbb
Tommy Pan <62233340+Tommyweige@users.noreply.github.com>
2026-08-21 17:52:25 +0800

    Day 8: export DQN network

--- breakout_rl/models/__init__.py
+++ breakout_rl/models/__init__.py
@@ -1,5 +1,6 @@
 """Neural-network building blocks used by the Breakout agent."""
 
 from breakout_rl.models.atari_cnn import AtariFeatureExtractor
+from breakout_rl.models.dqn import DQNNetwork
 
-__all__ = ["AtariFeatureExtractor"]
+__all__ = ["AtariFeatureExtractor", "DQNNetwork"]
